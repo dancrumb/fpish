48697e12f9441c77f5d7a5660a3c25e8b642620b
Dan Rumney <dan@sutro.xyz>
2024-05-06 16:09:39 -0500

    test(coverage): exclude docs directory from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
+    
         coverage: {
+            include: ['src/**'],
             enabled: true,
             reporter: ['html', 'lcov', 'cobertura'],
         },
