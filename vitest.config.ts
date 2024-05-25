c3b553dc354fc33e0ae6ccdf4faa2ff92b5eef74
Dan Rumney <dan@sutro.xyz>
2024-05-25 16:09:21 -0500

    test(coverage): switch to istanbul

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,16 +4,18 @@ export default defineConfig({
     test: {
 
         coverage: {
+            provider: 'istanbul',
             enabled: true,
             include: ['src/**'],
             exclude: ['coverage', 'dist', 'src/**/*.spec.ts'],
             reporter: ['html', 'lcov', 'cobertura', 'json'],
+            reportOnFailure: true,
             thresholds: {
                 autoUpdate: true,
-                lines: 93.44,
-                statements: 93.44,
-                branches: 98.79,
-                functions: 86.73
+                lines: 85.49,
+                statements: 85.3,
+                branches: 85.04,
+                functions: 85.18
             }
         },
     },
